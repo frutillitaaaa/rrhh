32f9863c4e72b5b48a834b412ed863e9b8977633
DenzelD1 <118927034+DenzelD1@users.noreply.github.com>
2025-06-20 14:20:02 -0400

    Arreglo de error
    Se modificó el código de departamentos y se cambió el nombre de la carpeta [id] a [area], puesto que  la búsqueda por unidad es mediante el área.

--- recursos_humanos/app/api/departamentos/[area]/route.ts
+++ recursos_humanos/app/api/departamentos/[area]/route.ts
@@ -5,33 +5,39 @@ interface Params {
     area: string;
 }
 
-export async function GET(request: Request, context: { params: Params }) {
+export async function GET(request: Request, { params }: { params: Params }) {
     try {
-        const area = decodeURIComponent(context.params.area);
+        const area = params.area;
+        console.log(`Buscando departamento con el área: "${area}"`);
         const departamento = await obtenerDepartamentoPorArea(area);
+        
         if (!departamento) {
+            console.log(`No se encontró el departamento para el área: "${area}"`);
             return NextResponse.json({ message: 'Departamento no encontrado' }, { status: 404 });
         }
+
         return NextResponse.json(departamento);
     } catch (error) {
         return NextResponse.json({ message: 'Error al obtener el departamento', error: (error as Error).message }, { status: 500 });
     }
 }
 
-export async function PUT(request: Request, context: { params: Params }) {
+export async function PUT(request: Request, { params }: { params: Params }) {
     try {
-        const area = decodeURIComponent(context.params.area);
         const data = await request.json();
+        const area = params.area;
+        
         await actualizarDepartamento(area, data);
+        
         return NextResponse.json({ message: 'Departamento actualizado con éxito' });
     } catch (error) {
         return NextResponse.json({ message: 'Error al actualizar el departamento', error: (error as Error).message }, { status: 400 });
     }
 }
 
-export async function DELETE(request: Request, context: { params: Params }) {
+export async function DELETE(request: Request, { params }: { params: Params }) {
     try {
-        const area = decodeURIComponent(context.params.area);
+        const area = params.area;
         await eliminarDepartamento(area);
         return new NextResponse(null, { status: 204 });
     } catch (error) {
